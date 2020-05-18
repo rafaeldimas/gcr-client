1bbb3ddcb998dc0bb5ed44f942919dfac27fecf7
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2020-05-17 23:26:30 -0300

    Otimizado rota de redirecionamento para login.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
+Route::redirect('/', 'login');
 
 Auth::routes([
     'register' => false,
